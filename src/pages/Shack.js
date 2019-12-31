f6144f1514008630dff486cc572604e5b28960ea
Kelly H <kellyaccnts@gmail.com>
2019-12-30 21:05:08 -0700

    shack conditional rendering/first "puzzle"

--- src/pages/Shack.js
+++ src/pages/Shack.js
@@ -27,6 +27,34 @@ const Shack = props => {
       setInteractText("You find nothing further of note.");
     }
   };
+  const lookDoor = () => {
+    if (state.inv["hasStone"] && !state.inv["shackOpen"]) {
+      setState(prev => ({
+        ...prev,
+        inv: { ...prev.inv, shackOpen: true }
+      }));
+      setInteractText(
+        "With only a moment of hesitation, you throw the rock through the door's window, shattering it. You reach inside and open the lock, and the door slowly drifts open."
+      );
+    } else {
+      setInteractText(
+        "The door is heavy, and it refuses to open. Through the small diamond-shaped window, you can see the deadbolt locked in place. Perhaps if you could break the window..."
+      );
+    }
+  };
+  const shackStatus = () => {
+    return state.inv["shackOpen"] ? (
+      <span>
+        The door <Travel>into the shack</Travel> sits open, bobbing slightly in
+        the wind.
+      </span>
+    ) : (
+      <span>
+        The <Sub c={lookDoor}>door to the shack</Sub> is closed, and has clearly
+        not been open in quite some time.
+      </span>
+    );
+  };
   return (
     <>
       <h2>Abandoned Shack</h2>
@@ -37,7 +65,7 @@ const Shack = props => {
         windows are painted with permanent haze and{" "}
         <Sub c={lookRubble}>a pile of rubble</Sub> sits adjacent, either a
         forgotten repair or the natural accumulation of debris that slowly
-        engulfs any house. Somewhere in the distance, a{" "}
+        engulfs any house. {shackStatus()} Somewhere in the distance, a{" "}
         <Sub c={lookGull}>gull</Sub> cries out. The path you trod to get here
         winds back down, inviting a{" "}
         <Travel to={() => setState(prev => ({ ...prev, page: <Seaside /> }))}>
