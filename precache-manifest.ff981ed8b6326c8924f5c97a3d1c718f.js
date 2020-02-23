72b4d64ec673da2f4109495e40f55e7dcb161d39
Kelly H <kellyaccnts@gmail.com>
2020-02-23 14:27:45 -0700

    Updates

---
+++ precache-manifest.ff981ed8b6326c8924f5c97a3d1c718f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d11b0832086f7bacd88b9a0dddc563dd",
+    "url": "/interactive-story/index.html"
+  },
+  {
+    "revision": "7075a83ed1837bcef465",
+    "url": "/interactive-story/static/css/main.552ca70c.chunk.css"
+  },
+  {
+    "revision": "3f9fa391b4bf6a647477",
+    "url": "/interactive-story/static/js/2.a269681f.chunk.js"
+  },
+  {
+    "revision": "7075a83ed1837bcef465",
+    "url": "/interactive-story/static/js/main.f452dcc1.chunk.js"
+  },
+  {
+    "revision": "647314daa60a0702029f",
+    "url": "/interactive-story/static/js/runtime-main.f01de4ad.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/interactive-story/precache-manifest.ff981ed8b6326c8924f5c97a3d1c718f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/interactive-story/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
